b5919c3f9623154502d7ac0fbde972f512ad519f
David Suh <davidisuh93@gmail.com>
2020-11-19 16:38:15 -0500

    added move method

--- Towers/lib/towers.rb
+++ Towers/lib/towers.rb
@@ -6,7 +6,13 @@ class Towers
         @towers[0] = [1, 2, 3, 4]
     end
 
-    def won?
-        return true if self.towers[1] == [1, 2, 3, 4] || self.towers[2] == [1, 2, 3, 4]
+    def move(old_index, new_index)
+        if old_index >= 0 && old_index < 3 && new_index >= 0  && new_index < 3 && old_index != new_index
+            towers[new_index].unshift(towers[old_index].shift)
+        end
     end
+
+    # def won?
+    #     return true if self.towers[1] == [1, 2, 3, 4] || self.towers[2] == [1, 2, 3, 4]
+    # end
 end
--- Towers/spec/towers_spec.rb
+++ Towers/spec/towers_spec.rb
@@ -8,15 +8,25 @@ describe Towers do
         end
     end
 
-    # describe "#won?" do
-    #     it "should return true if all disks were moved to the middle or last tower and are in order from smallest to largest" do
-    #         let(:game)
-    #         game.towers = [[], [], [1,2,3,4]]
-    #         expect(game.won?).to be true
-    #         game.towers = [[], [1, 2, 3, 4], []]
-    #         expect(game.won?).to be true
-    #     end
-    # end
+    describe "#move" do
+        it "should receive old_index and new_index as arguments" do
+            expect {game.move(0, 1)}.to_not raise_error
+        end
+
+        it "should take disk from beginning of one array and move it to another array" do
+            expect(game.move(0, 1)).to_not eq(game.towers)
+        end  
+    end
+
+    describe "#won?" do
+        it "should return true if all disks were moved to the middle or last tower and are in order from smallest to largest" do
+            let(:game)
+            game.towers = [[], [], [1,2,3,4]]
+            expect(game.won?).to be true
+            game.towers = [[], [1, 2, 3, 4], []]
+            expect(game.won?).to be true
+        end
+    end
 
 end
         
